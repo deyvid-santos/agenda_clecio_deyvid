e50b644e5222118e6aa4936aa7bc54e01043144b
Deyvid Santos <72878515+deyvid-santos@users.noreply.github.com>
2021-06-14 21:41:18 -0300

    INSERTS
    Após a criação do banco agendadeyvid; e depois da primeira execução do Spring App. Adicione os INSERTS para gerar as lista dos formulários.

---
+++ INSERTS.sql
@@ -0,0 +1,47 @@
+##INSERTS IMPORTANTES
+use agendadeyvid;
+
+##SEXO
+Insert into `sexo` (`idsexo`, `sexo`) values (NULL, 'Masculino');
+Insert into `sexo` (`idsexo`, `sexo`) values (NULL, 'Feminino');
+Insert into `sexo` (`idsexo`, `sexo`) values (NULL, 'Outros');
+
+##SANGUES
+Insert into `sangue` (`idsangue`, `sangue`) values (NULL, 'A+');
+Insert into `sangue` (`idsangue`, `sangue`) values (NULL, 'A-');
+Insert into `sangue` (`idsangue`, `sangue`) values (NULL, 'B+');
+Insert into `sangue` (`idsangue`, `sangue`) values (NULL, 'B-');
+Insert into `sangue` (`idsangue`, `sangue`) values (NULL, 'AB+');
+Insert into `sangue` (`idsangue`, `sangue`) values (NULL, 'AB-');
+Insert into `sangue` (`idsangue`, `sangue`) values (NULL, 'O+');
+Insert into `sangue` (`idsangue`, `sangue`) values (NULL, 'O-');
+Insert into `sangue` (`idsangue`, `sangue`) values (NULL, 'Não sei');
+
+##ESTADOS
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Acre');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Alagoas');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Amapá');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Amazonas');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Bahia');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Ceará');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Distrito Federal');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Espírito Santo');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Goiás');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Maranhão');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Mato Grosso');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Mato Grosso do Sul');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Minas Gerais');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Pará');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Paraíba');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Paraná');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Pernambuco');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Piauí');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Rio de Janeiro');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Rio Grande do Norte');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Rio Grande do Sul');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Rondônia');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Roraima');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Santa Catarina');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'São Paulo');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Sergipe');
+Insert into `estado` (`idestado`, `estado`) values (NULL, 'Tocantins');
